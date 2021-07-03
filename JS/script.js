a11e625dd50affcf3a36f1a14acdb233d9717ffc
Dfrmn <danikfurman2010@gmail.com>
2021-07-03 15:23:35 +0300

    2

--- JS/script.js
+++ JS/script.js
@@ -1,49 +1,62 @@
-let user = {
-    birthYear : +prompt(`Введите свой год рождения`) ,
-    currentCity : prompt(`Введите город , в котором проживаете`), 
-    favoriteSport : prompt(`Укажите ваш любимый спорт`),
+let yearOfBirth = +prompt(`Введите свой год рождения`) ;
+
+if(yearOfBirth){
+    let age=`Ваш возраст ` + (2021-yearOfBirth); 
+    let city = prompt(`Введите город , в котором проживаете`);
+
+    if(city){
+        switch (city) {
+            case `Киев`:
+                cityType=`Ты живешь в столице Украины`;
+                break;
+            case `Москва`:
+                alert(`Ты живешь в столице России`);
+                break;
+            case `Минск`:
+                cityType=`Ты живешь в столице Белоруссии`;
+                break;   
+            default:
+                cityType=`Ты живешь в городе ${city}`;
+        }
+
+        let favSport = prompt(`Укажите ваш любимый спорт`);
+
+        if(favSport){
+            switch (favSport) {
+                case `Баскетбол`:
+                    sportType=`Круто! Хочешь стать как Майкл Джордан?`;
+                    break;
+                case `Футбол`:
+                    sportType=`Круто! Хочешь стать как Лионель Месси?`;
+                    break;
+                case `Бокс`:
+                    sportType=`Круто! Хочешь стать как Виталий Кличко?`;
+                    break;
+                    default:
+                    sportType=`Круто! Ты занимаешься видом спорта:  ${favSport}`;
+            }
+
+            let user={
+                age, 
+                cityType,
+                sportType
+            }
+            
+            alert(user.age + "\n" + user.cityType + "\n" + user.sportType);
+            
+        }
+
+        else{
+            alert(`Жаль, что Вы не захотели указать свой любимый спорт`);
+        }
+        
+    }
+
+    else{
+        alert(`Жаль, что Вы не захотели ввести свой город`);
+    }
 }
 
-if (user.birthYear) {
-    alert(`Ваш возраст ${Math.abs(user.birthYear -= 2021)}`);    
-} else {
+else{
     alert(`Жаль , что вы не указали свой возраст`);
 }
-
-
-switch (user.currentCity) {
-    case `киев`:
-        alert(`Ты живешь в столице Украины`);
-        break;
-    case `москва`:
-        alert(`Ты живешь в столице России`);
-        break;
-    case `минск`:
-        alert(`Ты живешь в столице Белоруссии`);
-        break;   
-    case userCurrentCity:
-       alert(`ты живешь в городе ${userCurrentCity}`);
-        break;         
-    default:
-           alert(`Жаль, что Вы не захотели ввести свой город`);
-        break;
-}
-
-switch (user.favoriteSport) {
-    case `баскетбол`:
-        alert(`Круто! Хочешь стать как Майкл Джордан?)`);
-        break;
-    case `футбол`:
-        alert(`Круто! Хочешь стать как Лионель Месси?)`);
-        break;
-    case `бокс`:
-        alert(`Круто! Хочешь стать как Виталий Кличко?)`);
-        break;
-    default:
-        alert(`Жаль, что Вы не захотели указать свой любимый спорт`);
-        break;
-}
-
-// for (let key in user) {
-//     alert(user[key]);
-// }
